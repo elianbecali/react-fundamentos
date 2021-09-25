5a25a2b64daf24c62988dffd99c6a07420833405
Elian Becali <elianbecaliaguiar@gmail.com>
2021-09-25 10:15:21 -0400

    feat: create post list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import Header from './Header';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { title: 'Title#04', subtitle: 'Sub#04', likes: 60 },  
+  ]);
+
+  function handleRefresh() {
+    setPosts(prevState => [...prevState, {
+      id: Math.random(),
+      title: `Title#0${prevState.length + 1}`,
+      subtitle: `Title#0${prevState.length + 1}`,
+      likes: 50
+    }])
+  }
+
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Posts da semana</h2>
+        <h2>Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 01",
-          subtitle: "Subtítulo da notícia 01"
-        }}
-      />
-      <Post
-        likes={10}
-        post={{
-          title: "Título da notícia 02",
-          subtitle: "Subtítulo da notícia 02"
-        }}
-      />
-      <Post
-        likes={60}
-        post={{
-          title: "Título da notícia 03",
-          subtitle: "Subtítulo da notícia 03"
-        }}
-      />
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle
+          }}
+        />
+      ))}
     </>
   );
 }
